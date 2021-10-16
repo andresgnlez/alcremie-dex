03209926b85bafda8426a189aa536da708827753
Andrés González <kyrie@Kyri3-Desktop.localdomain>
2021-10-16 18:11:10 +0200

    adds favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
-import Head from 'next/head';
-
 const Home: React.FC = () => (
   <div>
-    <Head>
-      <title>Welcome</title>
-    </Head>
     <h1>Welcome to Vizzuality Front End scaffold project.</h1>
     <p>Remember to edit:</p>
     <ul>
